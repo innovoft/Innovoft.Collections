594eff2d3f058b6810bb9121273a0cc9beb14824
Steven Retz <sretz@innovoft.com>
2018-03-23 18:20:19 -0300

    added test RedBlackTree4321Test

--- Innovoft.Collections.UnitTests/RedBlackTreeTests.cs
+++ Innovoft.Collections.UnitTests/RedBlackTreeTests.cs
@@ -49,5 +49,20 @@ namespace Innovoft.Collections.UnitTests
 			Assert.IsTrue(tree.Contains(2));
 			Assert.IsTrue(tree.Contains(3));
 		}
+
+		[TestMethod]
+		public void RedBlackTree4321Test()
+		{
+			var tree = new RedBlackTree<int, int>(new ComparableAscendingComparer<int>().Compare);
+			tree.Add(4, 4);
+			tree.Add(3, 3);
+			tree.Add(2, 2);
+			tree.Add(1, 1);
+
+			Assert.AreEqual(1, tree.GetMinValue());
+			Assert.AreEqual(4, tree.GetMaxValue());
+			Assert.IsTrue(tree.Contains(2));
+			Assert.IsTrue(tree.Contains(3));
+		}
 	}
 }
