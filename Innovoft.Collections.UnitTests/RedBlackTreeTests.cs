ac89c64a9a9927c722df3de8a070dd401f1d12f4
Steven Retz <sretz@innovoft.com>
2018-03-23 18:10:23 -0300

    added tests RedBlackTreeTests added reference Innovoft

---
+++ Innovoft.Collections.UnitTests/RedBlackTreeTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Innovoft.Collections;
+
+namespace Innovoft.Collections.UnitTests
+{
+	[TestClass]
+	public class RedBlackTreeTests
+	{
+		[TestMethod]
+		public void RedBlackTree123Test()
+		{
+			var tree = new RedBlackTree<int, int>(new ComparableAscendingComparer<int>().Compare);
+			tree.Add(1, 1);
+			tree.Add(2, 2);
+			tree.Add(3, 3);
+
+			Assert.AreEqual(1, tree.GetMinValue());
+			Assert.AreEqual(3, tree.GetMaxValue());
+			Assert.IsTrue(tree.Contains(2));
+		}
+	}
+}
