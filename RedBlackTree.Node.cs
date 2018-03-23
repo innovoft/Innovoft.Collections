c2a8a11783ccf3e1314ad7d2944d28a854ed470f
Steven Retz <sretz@innovoft.com>
2018-03-23 14:30:30 -0300

    started class RedBlackTree

---
+++ RedBlackTree.Node.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Innovoft.Collections
+{
+	partial class RedBlackTree<TKey, TValue>
+	{
+		private sealed class Node
+		{
+			#region Fields
+			private readonly TKey key;
+			private TValue value;
+			private bool red;
+			private Node less;
+			private Node more;
+			#endregion Fields
+
+			#region Constructors
+			public Node(TKey key, TValue value)
+			{
+				this.key = key;
+				this.value = value;
+			}
+			#endregion Constructors
+
+			#region Properties
+			public TKey Key => key;
+			public TValue Value { get => this.value; set => this.value = value; }
+			public bool Red { get => red; set => red = value; }
+			public Node Less { get => less; set => less = value; }
+			public Node More { get => more; set => more = value; }
+			#endregion Properties
+		}
+	}
+}
---
+++ RedBlackTree.cs
@@ -0,0 +1,414 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Innovoft.Collections
+{
+	public sealed partial class RedBlackTree<TKey, TValue>
+	{
+		#region Fields
+		private readonly Comparison<TKey> comparer;
+
+		private Node tree;
+		private int count;
+		#endregion Fields
+
+		#region Constructors
+		public RedBlackTree(Comparison<TKey> comparer)
+		{
+			this.comparer = comparer;
+		}
+		#endregion Constructors
+
+		#region Properties
+		public Comparison<TKey> Comparer => comparer;
+
+		public int Count => count;
+		#endregion Properties
+
+		#region Methods
+		public void Clear()
+		{
+			tree = null;
+			count = 0;
+		}
+
+		public void Add(TKey key, TValue value)
+		{
+			if (!TryAdd(key, value))
+			{
+				throw new Exception();
+			}
+		}
+
+		public bool TryAdd(TKey key, TValue value)
+		{
+			if (tree == null)
+			{
+				tree = new Node(key, value);
+				count = 1;
+				return true;
+			}
+
+			var node = tree;
+			var parent = default(Node);
+			var grand = default(Node);
+			var great = default(Node);
+			int compared;
+			do
+			{
+				compared = comparer(key, node.Key);
+				if (compared == 0)
+				{
+					return false;
+				}
+				great = grand;
+				grand = parent;
+				parent = node;
+				node = compared < 0 ? node.Less : node.More;
+			}
+			while (node != null);
+			node = new Node(key, value);
+			if (compared < 0)
+			{
+				parent.Less = node;
+			}
+			else
+			{
+				parent.More = node;
+			}
+			++count;
+
+			throw new NotImplementedException();
+		}
+
+		private void AddBalance(Node node, Node parent, Node grand)
+		{
+			throw new NotImplementedException();
+		}
+
+		private void RotateLess()
+		{
+			throw new NotImplementedException();
+		}
+
+		private void RotateMore()
+		{
+			throw new NotImplementedException();
+		}
+
+		#region Min
+		public void GetMin(out TKey key, out TValue value)
+		{
+			if (tree == null)
+			{
+				throw new InvalidOperationException();
+			}
+
+			var node = tree;
+			while (true)
+			{
+				if (node.Less != null)
+				{
+					node = node.Less;
+					continue;
+				}
+
+				key = node.Key;
+				value = node.Value;
+				return;
+			}
+		}
+
+		public TKey GetMinKey()
+		{
+			if (tree == null)
+			{
+				throw new InvalidOperationException();
+			}
+
+			var node = tree;
+			while (true)
+			{
+				if (node.Less != null)
+				{
+					node = node.Less;
+					continue;
+				}
+
+				return node.Key;
+			}
+		}
+
+		public TValue GetMinValue()
+		{
+			if (tree == null)
+			{
+				throw new InvalidOperationException();
+			}
+
+			var node = tree;
+			while (true)
+			{
+				if (node.Less != null)
+				{
+					node = node.Less;
+					continue;
+				}
+
+				return node.Value;
+			}
+		}
+
+		public bool TryGetMin(out TKey key, out TValue value)
+		{
+			if (tree == null)
+			{
+				key = default(TKey);
+				value = default(TValue);
+				return false;
+			}
+
+			var node = tree;
+			while (true)
+			{
+				if (node.Less != null)
+				{
+					node = node.Less;
+					continue;
+				}
+
+				key = node.Key;
+				value = node.Value;
+				return true;
+			}
+		}
+
+		public bool TryGetMin(out TKey key)
+		{
+			if (tree == null)
+			{
+				key = default(TKey);
+				return false;
+			}
+
+			var node = tree;
+			while (true)
+			{
+				if (node.Less != null)
+				{
+					node = node.Less;
+					continue;
+				}
+
+				key = node.Key;
+				return true;
+			}
+		}
+
+		public bool TryGetMin(out TValue value)
+		{
+			if (tree == null)
+			{
+				value = default(TValue);
+				return false;
+			}
+
+			var node = tree;
+			while (true)
+			{
+				if (node.Less != null)
+				{
+					node = node.Less;
+					continue;
+				}
+
+				value = node.Value;
+				return true;
+			}
+		}
+		#endregion Min
+
+		#region Max
+		public void GetMax(out TKey key, out TValue value)
+		{
+			if (tree == null)
+			{
+				throw new InvalidOperationException();
+			}
+
+			var node = tree;
+			while (true)
+			{
+				if (node.More != null)
+				{
+					node = node.More;
+					continue;
+				}
+
+				key = node.Key;
+				value = node.Value;
+				return;
+			}
+		}
+
+		public TKey GetMaxKey()
+		{
+			if (tree == null)
+			{
+				throw new InvalidOperationException();
+			}
+
+			var node = tree;
+			while (true)
+			{
+				if (node.More != null)
+				{
+					node = node.More;
+					continue;
+				}
+
+				return node.Key;
+			}
+		}
+
+		public TValue GetMaxValue()
+		{
+			if (tree == null)
+			{
+				throw new InvalidOperationException();
+			}
+
+			var node = tree;
+			while (true)
+			{
+				if (node.More != null)
+				{
+					node = node.More;
+					continue;
+				}
+
+				return node.Value;
+			}
+		}
+
+		public bool TryGetMax(out TKey key, out TValue value)
+		{
+			if (tree == null)
+			{
+				key = default(TKey);
+				value = default(TValue);
+				return false;
+			}
+
+			var node = tree;
+			while (true)
+			{
+				if (node.More != null)
+				{
+					node = node.More;
+					continue;
+				}
+
+				key = node.Key;
+				value = node.Value;
+				return true;
+			}
+		}
+
+		public bool TryGetMax(out TKey key)
+		{
+			if (tree == null)
+			{
+				key = default(TKey);
+				return false;
+			}
+
+			var node = tree;
+			while (true)
+			{
+				if (node.More != null)
+				{
+					node = node.More;
+					continue;
+				}
+
+				key = node.Key;
+				return true;
+			}
+		}
+
+		public bool TryGetMax(out TValue value)
+		{
+			if (tree == null)
+			{
+				value = default(TValue);
+				return false;
+			}
+
+			var node = tree;
+			while (true)
+			{
+				if (node.More != null)
+				{
+					node = node.More;
+					continue;
+				}
+
+				value = node.Value;
+				return true;
+			}
+		}
+		#endregion Max
+
+		public bool Contains(TKey key)
+		{
+			if (tree == null)
+			{
+				return false;
+			}
+
+			var node = tree;
+			while (true)
+			{
+				var compared = comparer(key, node.Key);
+				if (compared == 0)
+				{
+					return true;
+				}
+				node = compared < 0 ? node.Less : node.More;
+				if (node == null)
+				{
+					return false;
+				}
+			}
+		}
+
+		public bool TryGetValue(TKey key, out TValue value)
+		{
+			if (tree == null)
+			{
+				value = default(TValue);
+				return false;
+			}
+
+			var node = tree;
+			while (true)
+			{
+				var compared = comparer(key, node.Key);
+				if (compared == 0)
+				{
+					value = node.Value;
+					return true;
+				}
+				node = compared < 0 ? node.Less : node.More;
+				if (node == null)
+				{
+					value = default(TValue);
+					return false;
+				}
+			}
+		}
+		#endregion Methods
+	}
+}
