98e7f41ce45912a2d4833bfe8134375a5651849f
Steven Retz <sretz@innovoft.com>
2018-03-23 17:31:14 -0300

    started UnitTests Innovoft.Collections.UnitTests

---
+++ Innovoft.Collections.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Innovoft.Collections.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("Innovoft.Collections.UnitTests")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e2f4594b-628d-4853-b214-a2ff2eb1a964")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0." + Build.SourceControl.Revision)]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
