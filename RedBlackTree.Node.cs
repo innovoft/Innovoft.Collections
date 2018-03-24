7685e511d5a15c5b79278fc19398691992f8f375
Steven Retz <sretz@innovoft.com>
2018-03-24 12:17:59 -0300

    modifying class inheritance private sealed class Node : Node<TKey, TValue>

--- RedBlackTree.Node.cs
+++ RedBlackTree.Node.cs
@@ -6,47 +6,41 @@ namespace Innovoft.Collections
 {
 	partial class RedBlackTree<TKey, TValue>
 	{
-		private sealed class Node
+		private sealed class Node : Node<TKey, TValue>
 		{
 			#region Fields
-			private readonly TKey key;
-			private TValue value;
 			private bool red;
 			private Node parent;
 			private Node less;
 			private Node more;
-			#endregion Fields
+			#endregion //Fields
 
 			#region Constructors
 			/// <summary>
 			/// Creates a Black root
 			/// </summary>
 			public Node(TKey key, TValue value)
+				: base(key, value)
 			{
-				this.key = key;
-				this.value = value;
 			}
 
 			/// <summary>
 			/// Creates a Red child
 			/// </summary>
 			public Node(TKey key, TValue value, Node parent)
+				: base(key, value)
 			{
-				this.key = key;
-				this.value = value;
 				this.red = true;
 				this.parent = parent;
 			}
-			#endregion Constructors
+			#endregion //Constructors
 
 			#region Properties
-			public TKey Key => key;
-			public TValue Value { get => this.value; set => this.value = value; }
 			public bool Red { get => red; set => red = value; }
 			public Node Parent { get => parent; set => parent = value; }
 			public Node Less { get => less; set => less = value; }
 			public Node More { get => more; set => more = value; }
-			#endregion Properties
+			#endregion //Properties
 
 			#region Methods
 			public override string ToString()
@@ -83,7 +77,7 @@ namespace Innovoft.Collections
 				}
 				return string.Join("|",	key, value, red ? "R" : "B", lessKey, moreKey, parentKey, direction);
 			}
-			#endregion Methods
+			#endregion //Methods
 		}
 	}
 }
