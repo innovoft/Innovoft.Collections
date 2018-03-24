7a79f720c3af380ffd0d070ef842b034f0362bcd
Steven Retz <sretz@innovoft.com>
2018-03-24 12:41:45 -0300

    updated inheritance public sealed class Node : TreeNode<TKey, TValue, Node>

--- RedBlackTree.Node.cs
+++ RedBlackTree.Node.cs
@@ -6,20 +6,17 @@ namespace Innovoft.Collections
 {
 	partial class RedBlackTree<TKey, TValue>
 	{
-		private sealed class Node : Node<TKey, TValue>
+		public sealed class Node : TreeNode<TKey, TValue, Node>
 		{
 			#region Fields
 			private bool red;
-			private Node parent;
-			private Node less;
-			private Node more;
 			#endregion //Fields
 
 			#region Constructors
 			/// <summary>
 			/// Creates a Black root
 			/// </summary>
-			public Node(TKey key, TValue value)
+			internal Node(TKey key, TValue value)
 				: base(key, value)
 			{
 			}
@@ -27,22 +24,19 @@ namespace Innovoft.Collections
 			/// <summary>
 			/// Creates a Red child
 			/// </summary>
-			public Node(TKey key, TValue value, Node parent)
-				: base(key, value)
+			internal Node(TKey key, TValue value, Node parent)
+				: base(key, value, parent)
 			{
 				this.red = true;
-				this.parent = parent;
 			}
 			#endregion //Constructors
 
 			#region Properties
-			public bool Red { get => red; set => red = value; }
-			public Node Parent { get => parent; set => parent = value; }
-			public Node Less { get => less; set => less = value; }
-			public Node More { get => more; set => more = value; }
+			internal bool Red { get => red; set => red = value; }
 			#endregion //Properties
 
 			#region Methods
+#if DEBUG
 			public override string ToString()
 			{
 				object lessKey;
@@ -77,6 +71,7 @@ namespace Innovoft.Collections
 				}
 				return string.Join("|",	key, value, red ? "R" : "B", lessKey, moreKey, parentKey, direction);
 			}
+#endif //DEBUG
 			#endregion //Methods
 		}
 	}
