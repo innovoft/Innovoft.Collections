4bed9b2cc118ac931facbe75ddd070390457016d
Steven Retz <sretz@innovoft.com>
2018-03-24 13:18:13 -0300

    deleted TreeNode added method public Node Next()

--- RedBlackTree.Node.cs
+++ RedBlackTree.Node.cs
@@ -6,9 +6,12 @@ namespace Innovoft.Collections
 {
 	partial class RedBlackTree<TKey, TValue>
 	{
-		public sealed class Node : TreeNode<TKey, TValue, Node>
+		public sealed class Node : Node<TKey, TValue>
 		{
 			#region Fields
+			private Node parent;
+			private Node less;
+			private Node more;
 			private bool red;
 			#endregion //Fields
 
@@ -25,18 +28,22 @@ namespace Innovoft.Collections
 			/// Creates a Red child
 			/// </summary>
 			internal Node(TKey key, TValue value, Node parent)
-				: base(key, value, parent)
+				: base(key, value)
 			{
+				this.parent = parent;
 				this.red = true;
 			}
 			#endregion //Constructors
 
 			#region Properties
+			public Node Parent { get => parent; internal set => parent = value; }
+			public Node Less { get => less; internal set => less = value; }
+			public Node More { get => more; internal set => more = value; }
 			internal bool Red { get => red; set => red = value; }
 			#endregion //Properties
 
 			#region Methods
-#if DEBUG
+			#region Object
 			public override string ToString()
 			{
 				object lessKey;
@@ -69,9 +76,49 @@ namespace Innovoft.Collections
 					parentKey = null;
 					direction = null;
 				}
-				return string.Join("|",	key, value, red ? "R" : "B", lessKey, moreKey, parentKey, direction);
-			}
+#if DEBUG
+				return string.Join("|", key, value, red ? "R" : "B", lessKey, moreKey, parentKey, direction);
+#else //DEBUG
+				return string.Join("|",	key, value, lessKey, moreKey, parentKey, direction);
 #endif //DEBUG
+			}
+			#endregion //Object
+
+			public Node Next()
+			{
+				Node node;
+				if (more != null)
+				{
+					node = more;
+					while (true)
+					{
+						if (node.less != null)
+						{
+							node = node.less;
+							continue;
+						}
+
+						break;
+					}
+					return node;
+				}
+				node = this;
+				var parent = this.parent;
+				while (true)
+				{
+					if (parent == null)
+					{
+						return null;
+					}
+					if (parent.more == node)
+					{
+						node = parent;
+						parent = parent.Parent;
+						continue;
+					}
+					return parent;
+				}
+			}
 			#endregion //Methods
 		}
 	}
