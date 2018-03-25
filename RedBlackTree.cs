b1227b116dcb2db9cfb69cead1ba998bb7a759b0
Steven Retz <sretz@innovoft.com>
2018-03-25 16:57:29 -0300

    added method public void CopyKeysAscending(Node<TKey, TValue>[] nodes, int offset)

--- RedBlackTree.cs
+++ RedBlackTree.cs
@@ -1060,6 +1060,34 @@ namespace Innovoft.Collections
 			}
 		}
 
+		public void CopyKeysAscending(Node<TKey, TValue>[] nodes, int offset)
+		{
+			if (tree == null)
+			{
+				return;
+			}
+
+			var node = tree;
+			while (true)
+			{
+				if (node.Less != null)
+				{
+					node = node.Less;
+					continue;
+				}
+
+				break;
+			}
+			while (true)
+			{
+				nodes[offset++] = new Node<TKey, TValue>(node);
+				if (!node.TryNext(out node))
+				{
+					return;
+				}
+			}
+		}
+
 		public void CopyKeysAscending(TKey[] keys, int offset)
 		{
 			if (tree == null)
