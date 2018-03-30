6dc004a8b4931bba68f22426f4ae016f61ced741
Steven Retz <sretz@innovoft.com>
2018-03-29 21:00:34 -0300

    added class public class RedBlackNILLTreeTests

---
+++ Innovoft.Collections.UnitTests/RedBlackNILLTreeTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Innovoft.Collections;
+
+namespace Innovoft.Collections.UnitTests
+{
+	[TestClass]
+	public class RedBlackNILLTreeTests
+	{
+	}
+}
