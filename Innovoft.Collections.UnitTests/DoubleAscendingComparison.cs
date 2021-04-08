9744e837c8e56e981aa0ba262e712fa6bd599f13
Steven Retz <sretz@innovoft.com>
2021-04-07 23:41:31 -0300

    copied class from Innovoft to get project to compile

---
+++ Innovoft.Collections.UnitTests/DoubleAscendingComparison.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Innovoft.Collections
+{
+	public static class DoubleAscendingComparison
+	{
+		#region Methods
+		public static int Comparison(double x, double y)
+		{
+			if (x == y)
+			{
+				return 0;
+			}
+			if (x > y)
+			{
+				return +1;
+			}
+			else
+			{
+				return -1;
+			}
+		}
+		#endregion Methods
+	}
+}
